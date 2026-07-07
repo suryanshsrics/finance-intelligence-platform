b822b32f8a0f505ad54767d93a8adbb55f6a2c4a
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-07-07 23:43:47 +0530

    changed file name from users.py to user_routes.py

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 
 from app.database import Base, engine
 from app import models
-from app.api.v1 import users
+from app.api.v1 import user_routes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Finance Intelligence Platform', version='1.0.0')
 
 # Mount routers
-app.include_router(users.router, prefix="/api/v1/users", tags=["user"])
+app.include_router(user_routes.router, prefix="/api/v1/users", tags=["user"])
 
 @app.get("/")
 def root():
