d41e65e8ad5b2a57623d361c724d5711f37867b5
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-07-09 00:54:14 +0530

    added get all statements and get statement by id API

--- app/services/statement_services.py
+++ app/services/statement_services.py
@@ -15,8 +15,11 @@ def create_statement_service(statement: StatementCreate, db: Session):
     if statement.statement_start_date > statement.statement_end_date:
         raise HTTPException(status_code=400, detail="Start date cannot be after the end date. ")
     
+    if not statement.file_name or not statement.file_name.strip():
+        raise HTTPException(status_code=400, detail="file_name cannot be empty")
+    
     new_statement = Statement(user_id = statement.user_id, file_name = statement.file_name, bank_name = statement.bank_name
-    , account_number_last4 = statement.account_number_last_4, statement_start_date = statement.statement_start_date, statement_end_date = statement.statement_end_date)
+    , account_number_last4 = statement.account_number_last4, statement_start_date = statement.statement_start_date, statement_end_date = statement.statement_end_date)
 
     db.add(new_statement)
     try:
@@ -27,3 +30,26 @@ def create_statement_service(statement: StatementCreate, db: Session):
         raise
 
     return new_statement
+
+def get_statements_service(user_id: int, db: Session):
+    stmt = select(User).where(User.user_id == user_id)
+    user = db.execute(stmt).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found.")
+    
+    stmt_statement = select(Statement).where(Statement.user_id == user_id)
+    statements = db.execute(stmt_statement).scalars().all()
+    return statements
+
+def get_statement_by_id_service(user_id: int, statement_id: int, db: Session):
+    stmt = select(User).where(User.user_id == user_id)
+    user = db.execute(stmt).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found.")
+
+    stmt2 = select(Statement).where(Statement.user_id == user_id, 
+                                    Statement.statement_id == statement_id)
+    required_statement = db.execute(stmt2).scalar_one_or_none()
+    if required_statement is None:
+        raise HTTPException(status_code=404, detail=f"No statement with id: {statement_id}")
+    return required_statement
