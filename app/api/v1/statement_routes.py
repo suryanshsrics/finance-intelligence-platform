823c8fc8899e00cfbf33733c297d8452a3a0361e
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-07-09 00:54:14 +0530

    added get all statements and get statement by id API

--- app/api/v1/statement_routes.py
+++ app/api/v1/statement_routes.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.schemas.statement_schema import *
@@ -8,4 +9,14 @@ router = APIRouter()
 
 @router.post("/create-statement", response_model=StatementResponse, status_code=status.HTTP_201_CREATED)
 def create_statement(statement: StatementCreate, db: Session = Depends(get_db)):
-    return create_statement_service(statement, db) 
+    return create_statement_service(statement, db)
+
+
+@router.get("/users/{user_id}/statements", response_model=List[StatementResponse], status_code=status.HTTP_200_OK)
+def get_statements(user_id: int, db: Session = Depends(get_db)):
+    return get_statements_service(user_id, db)
+
+
+@router.get("/users/{user_id}/statement/{statement_id}", response_model=StatementResponse, status_code=status.HTTP_200_OK)
+def get_statement_by_id(user_id: int, statement_id: int, db: Session = Depends(get_db)):
+    return get_statement_by_id_service(user_id, statement_id, db)
