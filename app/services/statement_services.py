b50c013f599d53643d5192c24b524328176f2111
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-07-11 01:33:25 +0530

    Created statement upload endpoint and added text extract from pdf logic.

--- app/services/statement_services.py
+++ app/services/statement_services.py
@@ -1,9 +1,10 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, UploadFile
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.models.statement_model import Statement
 from app.models.user_model import User
 from app.schemas.statement_schema import StatementCreate
+from app.parsers.statement_parser import *
 from app.database import get_db
 
 def create_statement_service(statement: StatementCreate, db: Session):
@@ -53,3 +54,49 @@ def get_statement_by_id_service(user_id: int, statement_id: int, db: Session):
     if required_statement is None:
         raise HTTPException(status_code=404, detail=f"No statement with id: {statement_id}")
     return required_statement
+
+def statement_upload_service(user_id: int, file: UploadFile,
+                             password: str | None, db: Session):
+    # check if the user exists
+    stmt = select(User).where(User.user_id == user_id)
+    user = db.execute(stmt).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User does not exist.")
+    
+    # check if the uploaded file is a pdf
+    if not file.filename or not file.filename.lower().endswith('.pdf'):
+        raise HTTPException(status_code=400, detail='Only pdf files are allowed.')
+    
+    # check for empty file name
+    if not file.filename:
+        raise HTTPException(status_code=400, detail='No file selected.')
+    
+    try:
+        parsed_pdf = extract_text_from_pdf(
+            file.file,
+            password=password
+        )
+
+    except PdfPasswordRequiredError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+    except PdfIncorrectPasswordError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+    except PdfParsingError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+    return {
+    "message": "PDF uploaded successfully.",
+    "page_count": parsed_pdf['page_count'],
+    "text_preview": parsed_pdf['raw_text'][:1000]
+    }   
