14a3a3bfe740d71015be0c4079d96c01d9c10c6b
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-07-12 21:10:29 +0530

    added some parsing logic

--- app/parsers/statement_parser.py
+++ app/parsers/statement_parser.py
@@ -14,6 +14,10 @@ class PdfParsingError(Exception):
     """Raised when the PDF cannot be parsed."""
     pass
 
+class UnsupportedBankError(Exception):
+    """Raised when the uploaded bank statement is not supported"""
+    pass
+
 def extract_text_from_pdf(pdf_stream: BinaryIO, password: str | None = None):
     try:
         with pdfplumber.open(pdf_stream, password=password) as pdf:
@@ -25,6 +29,9 @@ def extract_text_from_pdf(pdf_stream: BinaryIO, password: str | None = None):
                     pages_text.append(text)
             
             full_text = "\n".join(pages_text)
+
+            with open("parsed_statement.txt", 'w', encoding='utf-8') as f:
+                f.write(full_text)
 
         return {
             'page_count': page_count,
@@ -38,4 +45,11 @@ def extract_text_from_pdf(pdf_stream: BinaryIO, password: str | None = None):
         raise PdfPasswordRequiredError("This PDF is password protected.")
     
     except Exception as e:
-        raise PdfParsingError(f"Unable to parse pdf: str(e)")
+        raise PdfParsingError(f"Unable to parse pdf: {e}")
+    
+def detect_bank(raw_text: str) -> str:
+    if "State Bank of India" in raw_text:
+        return "State Bank of India"
+    else:
+        raise UnsupportedBankError(f"This bank is unsupported.")
+    
