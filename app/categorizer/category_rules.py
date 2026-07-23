48bbcaaca40754044e5342db20b882eddd2f1d81
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-07-23 23:50:57 +0530

    Improved categorization logic

--- app/categorizer/category_rules.py
+++ app/categorizer/category_rules.py
@@ -1,79 +1,102 @@
 CATEGORY_RULES = {
     "Food": [
-        'SWIGGY',
-        'ZOMATO',
-        "DOMINO'S",
-        'BURGER KING',
-        'PIZZA HUT',
-        "MCDONALD'S",
-        'KFC',
-        'HUNGERBOX'
+        r"\bSWIGGY\b",
+        r"\bZOMATO\b",
+        r"\bHUNGERBOX\b",
+        r"\bEATCLUB\b",
+        r"\bDOMINO'?S\b",
+        r"\bBURGER\s+KING\b",
+        r"\bPIZZA\s+HUT\b",
+        r"\bMCDONALD'?S\b",
+        r"\bKFC\b",
+        r"\bBEHROUZ\b",
+        r"\bFAASSOS\b",
     ],
-    "Shopping": [
-        'AMAZON',
-        'FLIPKART',
-        'MYNTRA',
-        'MEESHO',
-        'AJIO',
-        'NYKAA'
-    ],
-    "Travel": [
-        'IRCTC',
-        'MAKEMYTRIP',
-        'YATRA',
-        'INDIGO',
-        'AIR INDIA'
-    ],
-    "Transport": [
-        'RAPIDO',
-        'UBER',
-        'OLA'
-    ],
-    "Fuel": [
-        'INDIAN OIL',
-        'IOCL',
-        'BPCL',
-        'HPCL'
-        'PETROL',
-        'DIESEL'
-    ],
-    "Recharge": [
-        'AIRTEL',
-        'JIO',
-        'TATA PLAY',
-        'BSNL'
-    ],
-    "Utilities": [
-        'ELECTRICITY',
-        'WATER',
-        'GAS',
-        'BILLDESK',
-        'UPPCL',
-        'WBSEDCL',
-        'PRONTO'
-    ],
-    "Entertainment": [
-        'NETFLIX',
-        'PRIME VIDEO',
-        'JIO HOTSTAR',
-        'SONY LIV',
-        'SPOTIFY',
-    ],
-    "Transfer": [
-        'UPI',
-        'NEFT',
-        'IMPS',
-        'RTGS'
-    ],
-    "Salary": [
-        'SALARY'
-    ],
-    "Interest": [
-        'INTEREST'
-    ],
-    "Charges": [
-        'CHARGE',
-        'GST'
-    ]
 
+    "Shopping": [
+        r"\bAMAZON\b",
+        r"\bFLIPKART\b",
+        r"\bMYNTRA\b",
+        r"\bMEESHO\b",
+        r"\bAJIO\b",
+        r"\bNYKAA\b",
+        r"\bZEPTO\b",
+        r"\bBLINKIT\b",
+        r"\bINSTAMART\b",
+        r"\bEKART\b"
+    ],
+
+    "Travel": [
+        r"\bIRCTC\b",
+        r"\bIRCTCTOURISM\b",
+        r"\bMAKEMYTRIP\b",
+        r"\bYATRA\b",
+        r"\bREDBUS\b",
+        r"\bINDIGO\b",
+        r"\bAIR\s+INDIA\b",
+    ],
+
+    "Transport": [
+        r"\bUBER\b",
+        r"\bOLA\b",
+        r"\bRAPIDO\b",
+        r"\bNAMMA\s+YATRI\b",
+    ],
+
+    "Fuel": [
+        r"\bINDIAN\s+OIL\b",
+        r"\bIOCL\b",
+        r"\bBPCL\b",
+        r"\bHPCL\b",
+        r"\bPETROL\b",
+        r"\bDIESEL\b",
+    ],
+
+    "Recharge": [
+        r"\bAIRTEL\b",
+        r"\bJIO\b",
+        r"\bBSNL\b",
+        r"\bVI\b",
+        r"\bVODAFONE\b",
+        r"\bIDEA\b",
+        r"\bBHIMRECHAR\b",
+        r"\bTATA\s+PLAY\b",
+    ],
+
+    "Utilities": [
+        r"\bELECTRICITY\b",
+        r"\bWATER\b",
+        r"\bGAS\b",
+        r"\bBILLDESK\b",
+        r"\bUPPCL\b",
+        r"\bWBSEDCL\b",
+        r"\bPRONTO\b",
+        r"\bBESCOM\b",
+        r"\bMSEDCL\b",
+        r"\bTANGEDCO\b",
+    ],
+
+    "Entertainment": [
+        r"\bNETFLIX\b",
+        r"\bPRIME\s+VIDEO\b",
+        r"\bHOTSTAR\b",
+        r"\bSONY\s+LIV\b",
+        r"\bSPOTIFY\b",
+        r"\bYOUTUBE\b",
+    ],
+
+    "Salary": [
+        r"\bSALARY\b",
+    ],
+
+    "Interest": [
+        r"\bINTEREST\b",
+    ],
+
+    "Charges": [
+        r"\bCHARGE\b",
+        r"\bGST\b",
+        r"\bPENALTY\b",
+        r"\bANNUAL\s+FEE\b",
+    ]
 }
