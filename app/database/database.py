a59693b81d236e3386089204b33b61d732423e9a
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-08-24 00:28:52 +0530

    Move db configuration to environment variables

--- app/database/database.py
+++ app/database/database.py
@@ -1,10 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.utils.settings import settings
 
-# DATABASE_URL = "postgresql://postgres:REDACTED    @localhost:5432/finance_intelligence"
+DATABASE_URL = URL.create(
+    drivername="postgresql+psycopg",
+    username=settings.database_user,
+    port=settings.database_port,
+    password=settings.database_password,
+    host=settings.database_host,
+    database=settings.database_name
+)
 
 # encode the url as '@' character in password is problemeatic
-DATABASE_URL = "postgresql+psycopg://postgres:REDACTED@localhost:5432/finance_intelligence"
 
 engine = create_engine(DATABASE_URL, echo=False)
 
---
+++ app/utils/settings.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    database_host: str
+    database_port: int
+    database_name: str
+    database_user: str
+    database_password: str 
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
