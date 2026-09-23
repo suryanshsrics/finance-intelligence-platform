9f49eb3622ca4ed579507f3e2b7bd5922539db32
Suryansh Srivastava <suryanshsrivastava912@gmail.com>
2026-09-24 00:06:20 +0530

    Implement public key caching; improve exception handling

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,6 +1,12 @@
 from fastapi.security import OAuth2AuthorizationCodeBearer
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 import jwt
+from jwt.exceptions import (InvalidTokenError,
+                            ExpiredSignatureError,
+                            InvalidSignatureError,
+                            InvalidAudienceError,
+                            InvalidIssuerError,
+                            DecodeError)
 
 from app.auth.keycloak import get_keycloak_public_key
 from app.utils.settings import settings
@@ -12,9 +18,25 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
 issuer = f"{settings.keycloak_url}/realms/{settings.keycloak_realm}"
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    header = jwt.get_unverified_header(token)
-    kid = header["kid"]
-    public_key = get_keycloak_public_key(kid)
+    try:
+        header = jwt.get_unverified_header(token)
+        kid = header["kid"]
+        public_key = get_keycloak_public_key(kid)
 
-    payload = jwt.decode(token, public_key, algorithms=["RS256"], issuer=issuer, audience="account")
-    return payload
+        payload = jwt.decode(token, public_key, algorithms=["RS256"], issuer=issuer, audience="account")
+        return payload
+
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token has expired.') from None
+    except InvalidAudienceError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token audience.') from None
+    except InvalidIssuerError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token issuer.') from None
+    except InvalidSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token signature.') from None
+    except DecodeError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token.') from None
+    except InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials.") from None
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials.') from None
--- app/auth/keycloak.py
+++ app/auth/keycloak.py
@@ -6,19 +6,30 @@ from app.utils.settings import settings
 
 # JWKS_URL_FORMAT = {KEYCLOAK_SERVER_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/certs
 
+JWKS_CACHE = {}
+
 def get_keycloak_public_key(kid: str):
     keycloak_jwks_url = f"{settings.keycloak_url}/realms/{settings.keycloak_realm}/protocol/openid-connect/certs"
-
+    if kid in JWKS_CACHE:
+        # kid_json = json.dumps(kid)
+        return JWKS_CACHE[kid]
+    
     response = httpx.get(keycloak_jwks_url)
+    # print('Fetching public key from KEYCLOAK')
     response.raise_for_status()
 
     data = response.json()
     keys = data["keys"]
 
     for key in keys:
+        kj = json.dumps(key)
+        public_key = RSAAlgorithm.from_jwk(kj)
+        JWKS_CACHE[key['kid']] = public_key
+
         if key["kid"] == kid:
-            key_json = json.dumps(key)
-            public_key = RSAAlgorithm.from_jwk(key_json)
+            # key_json = json.dumps(key)
+            # public_key = RSAAlgorithm.from_jwk(key_json)
+            # JWKS_CACHE[kid] = public_key
             return public_key
         
     raise ValueError(f"No Keycloak signing key found for kid: {kid}")
